72d09f1efc4eec9dc989043d26b2f7389a3a9a5b
tarasvolianskyi <t.volianskyi@aboutcode.us>
2018-03-15 12:49:38 +0200

    add Retrofit configuration

--- app/src/main/java/com/example/tarasvolianskyi/retrofitapplication/MainActivity.java
+++ app/src/main/java/com/example/tarasvolianskyi/retrofitapplication/MainActivity.java
@@ -3,11 +3,53 @@ package com.example.tarasvolianskyi.retrofitapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static java.lang.StrictMath.log;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrofit();
     }
+
+    private void retrofit() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        MessagesApi messagesApi = retrofit.create(MessagesApi.class);
+
+        Call<List<Message>> messages = messagesApi.messages();
+
+        messages.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                if (response.isSuccessful()) {
+                    int size = response.body().size();
+                    List<Message> data = response.body();
+                } else {
+                    int code = response.code();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+                Throwable error = t;
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/tarasvolianskyi/retrofitapplication/MessagesApi.java
@@ -0,0 +1,16 @@
+package com.example.tarasvolianskyi.retrofitapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by tarasvolianskyi on 15.03.18.
+ */
+
+public interface MessagesApi {
+
+    @GET("messages1.json")
+    Call<List<Message>> messages();
+}
